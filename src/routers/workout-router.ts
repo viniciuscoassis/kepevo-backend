d73de63706ecc77df3eed210fdbec20a0a621208
vinicius <vinicius.coassis@gmail.com>
2023-01-31 13:57:50 -0300

    feat: add auth in backend

--- src/routers/workout-router.ts
+++ src/routers/workout-router.ts
@@ -1,8 +1,10 @@
 import { getWorkouts, renameWorkout } from "@/controllers";
+import { authenticateToken } from "@/middlewares";
 import { Router } from "express";
 
 const WorkoutRouter = Router();
 
+WorkoutRouter.all("/*", authenticateToken);
 WorkoutRouter.get("/", getWorkouts);
 WorkoutRouter.put("/id", renameWorkout);
 
